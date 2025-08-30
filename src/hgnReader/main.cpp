94d214979fb532770f4a4e6fd79dfb694338b9b3
宇佑 蕭 <yuyoux7@gmail.com>
2025-08-31 00:53:01 +0800

    Update

--- src/hgnReader/main.cpp
+++ src/hgnReader/main.cpp
@@ -1,36 +1,108 @@
 #include <iostream>
 #include <fstream>
-
+#include "base64.hpp"
+constexpr auto _BFILE_ = "bfile.hgn";
 using namespace ::std;
 
 int main(int argc, char* argv[])
 {
 	if (argc == 2)
 	{
-		string Buff{}, fstr{}, allstring{}, laststring{};
-		ifstream ifile{};
-		ofstream ofile{};
-		ifile.open(argv[1]);
-		cout << argv[1];
-		if (ifile.is_open())
+		string Buff{}, fstr{}, allstring{}, tstr{}, ostr{}, bsstr{};
+		ifstream ifile{}, bifile{};
+		ofstream tofile{}, bfile{};
+		bool statstr{}, lock{};
+		bifile.open(_BFILE_);
+		if (bifile.is_open())
 		{
-			while (!ifile.eof())
+			while (!bifile.eof())
 			{
-				ifile >> fstr;
-				allstring += (fstr + "\n");
+				bifile >> Buff;
+				bsstr += Buff;
+				if (!bifile.eof())
+				{
+					bsstr += '\n';
+				}
+			}
+		}
+		bifile.close();
+		bsstr = base64::from_base64(bsstr);
+		ifile.open(argv[1]);
+		try {
+			if (ifile.is_open())
+			{
+				while (!ifile.eof())
+				{
+					ifile >> fstr;
+					if (fstr[0] == '#')
+					{
+						while (fstr[fstr.size() - 1] != '#')
+						{
+							ifile >> fstr;
+						}
+						ifile >> fstr;
+					}
+					allstring += fstr;
+					if (!ifile.eof())
+					{
+						allstring += '\n';
+					}
+				}
+			}
+			else {
+				lock = true;
+				throw invalid_argument("open fail");
+			}
+		}
+		catch (const invalid_argument& e)
+		{
+			cerr << e.what();
+			return -1;
+		}
+		try {
+			if (bsstr != allstring)
+			{
+				throw (-1);
+			}
+		}
+		catch (const int& n){
+			if (n == (-1))
+			{
+				cerr << "Data Error\nturn to new file";
 			}
 		}
 		ifile.close();
 		for (auto i = 0; i < allstring.size(); i++)
 		{
-			laststring += (allstring[i] ^ 'a');
+			if (allstring[i] == '<' || statstr) {
+				statstr = true;
+				if (allstring[i] != '\n')
+					tstr += allstring[i];
+				else
+					tstr += ' ';
+				if (allstring[i] == '>' && allstring[i - 1] == '/')
+				{
+					statstr = false;
+					ostr += (tstr + '\n');
+					tstr.clear();
+				}
+			}
 		}
-		laststring.pop_back();
-		ofile.open(argv[1]);
-		if (ofile.is_open())
+		ostr.pop_back();
+		if (!lock) {
+			tofile.open("test.hgn");
+			if (tofile.is_open())
+			{
+				tofile << ostr;
+			}
+		}
+		tofile.close();
+		std::string bstr = base64::to_base64(allstring);
+		bfile.open(_BFILE_);
+		if (bfile.is_open())
 		{
-			ofile << laststring;
+			bfile << bstr;
 		}
-		ofile.close();
+		bfile.close();
 	}
 }
